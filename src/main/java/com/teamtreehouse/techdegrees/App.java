8899d90978d9ac09a556d01a295391289f30d3d6
Yolis <yolisisabel28@gmail.com>
2025-01-07 14:48:28 -0800

    get route not working

--- src/main/java/com/teamtreehouse/techdegrees/App.java
+++ src/main/java/com/teamtreehouse/techdegrees/App.java
@@ -1,6 +1,10 @@
 package com.teamtreehouse.techdegrees;
 
 
+import com.google.gson.Gson;
+import com.teamtreehouse.techdegrees.Dao.Sql2oTodoDao;
+import com.teamtreehouse.techdegrees.Model.Todo;
+import com.teamtreehouse.techdegrees.Dao.TodoDao;
 import org.sql2o.Sql2o;
 
 import static spark.Spark.*;
@@ -8,26 +12,39 @@ import static spark.Spark.*;
 public class App {
 
     public static void main(String[] args) {
-        //our db
-        String datasource = "jdbc:h2:~/todos.db";
-        if(args.length > 0){
-            //if it doesn't pass both params
-            if(args.length != 2){
-                System.exit(0);
-            }
-            //writing out port from string number
-            //runs port we're running on
-            port(Integer.parseInt(args[0]));
-            datasource= args[1];
+
+//        //creates conn to db
+//        Sql2o sql2o = new Sql2o("jdbc:h2:~/todos.db;INIT=RUNSCRIPT from 'classpath:db/init.sql'");
+//        System.out.println("Database initialized successfully!");
+//
+//        //handle db operations
+//        TodoDao dao = new Sql2oTodoDao(sql2o);
+//        Gson gson = new Gson();
+//
+//        //get("/api/v1/todos", "application/json", (req, res) ->
+//                        dao.findAll(), gson::toJson);
+
+
+
+        try {
+            Sql2o sql2o = new Sql2o("jdbc:h2:~/todos.db;INIT=RUNSCRIPT from 'classpath:db/init.sql'");
+            System.out.println("Database initialized successfully!");
+
+            TodoDao dao = new Sql2oTodoDao(sql2o);
+            Gson gson = new Gson();
+
+            get("/api/v1/todos", "application/json", (req, res) ->
+                    dao.findAll(), gson::toJson);
+
+        } catch (Exception e) {
+            System.out.println("An error occurred: " + e.getMessage());
+            e.printStackTrace();
         }
 
-        //INSTANTIATING sql2o
-        //talking to the db
-        Sql2o sql2o = new Sql2o(
-                String.format("%s;INIT=RUNSCRIPT from 'classpath:db/init.sql'", datasource), "", "");
-
-        get("/blah", (req, res) -> "Hello!");
-
+        // Ensuring all responses are in JSON format
+        after((req, res) -> {
+            res.type("application/json");
+        });
     }
 
 }
