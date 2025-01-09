a7642d2cf8f6c82f341f0a030a66479ed5b156ee
Yolis <yolisisabel28@gmail.com>
2025-01-08 16:50:42 -0800

    new data stored in db from frontend

--- src/main/resources/public/scripts/controllers/main.js
+++ src/main/resources/public/scripts/controllers/main.js
@@ -6,16 +6,21 @@ angular.module('todoListApp')
   $scope.todos = Todo.query();
   
   $scope.addTodo = function() {
+    if (!$scope.newTodoName || $scope.newTodoName.trim() === '') {
+      alert("Task name cannot be empty!");
+      return;
+    }
+
     var todo = new Todo();
-    todo.name = 'New task!'
+    todo.name = $scope.newTodoName ? $scope.newTodoName.trim() : 'New task!';
+
     todo.completed = false;
 
-    todo.$save(function(saveTodo){
-    //on successful save, add the new to-do to the list
-        $scope.todos.unshift(todo);
-    }, function(error){
-        console.error("Failed to save the todo:", error);
-    })
+    todo.$save(function(savedTodo) {
+      $scope.todos.unshift(savedTodo); // Add the saved todo to the list
+      $scope.newTodoName = ''; // Clear the input box after saving
+    }, function(error) {
+      console.error("Failed to save the todo:", error);
+    });
   };
-  
 })
